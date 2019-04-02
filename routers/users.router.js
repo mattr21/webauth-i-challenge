8ca191004dde6efccf13ed2ff6a0552409e9f61a
mattr21 <matt.rothstein21@gmail.com>
2019-04-01 17:32:08 -0700

    added POST endpoint for registering a new user, hashed the password with bcryptjs

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('knex');
+const bcrypt = require('bcryptjs');
 
 const knexConfig = {
     client: 'sqlite3',
     connection: {
-        filename: './data/[db name].db3'
+        filename: './data/auth.db3'
     },
     useNullAsDefault: true,
 };
 
 const db = knex(knexConfig);
 
-// add endpoints
+// test to make sure server is setup
+router.get('/test', (req, res) => {
+    res.send('test');
+});
+
+// register a user
+router.post('/register', async (req, res) => {
+    try {
+        let user = req.body;
+
+        const hash = bcrypt.hashSync(user.password, 2);
+        user.password = hash;
+
+        const [id] = await db('users')
+            .insert(user);
+        
+        const saved = await db('users')
+            .where({id: id})
+            .first();
+
+        res.status(201).json(saved);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
