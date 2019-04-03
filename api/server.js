29e9189472d80b9ce4c58a697dd3d22a492ef06b
mattr21 <matt.rothstein21@gmail.com>
2019-04-02 17:15:06 -0700

    added sessions library, added/configured/used sessions in server.js, created a cookie when user logs in, and changed restricted middleware to use the session as a way to tell if a user is logged in

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const authRouter = require('../auth/auth-router.js')
 const usersRouter = require('../users/users-router.js')
 
 const server = express();
 
+const sessionConfig = {
+    name: 'monster',
+    secret: 'keep it secret, keep it safe!',
+    cookie: {
+        maxAge: 1000 * 60 * 10, // how long the sessions lasts (in milliseconds). 1 sec * 60 sec * 10 min 
+        secure: false, // use cookie over https only. In production we change it to true with an env variable
+        httpOnly: true, // JS can't access the cookie on the client
+    },
+    resave: false, // avoid recreating unchanged sessions
+    saveUninitialized: false, // GDPR compliance - can use cookies by default?
+};
+
 server.use(helmet());
 server.use(express.json());
+server.use(session(sessionConfig))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -46,6 +46,8 @@ router.post('/login', async (req, res) => {
             .first();
 
         if (user && bcrypt.compareSync(password, user.password)) {
+            req.session.user = user;
+
             res.status(200).json({ message: `Welcome ${user.username}!` });
         } else {
             res.status(401).json({ message: `YOU SHALL NOT PASS!!!` });
